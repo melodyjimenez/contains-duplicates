33a23d41c9fb98c82a4720d3f7a2105aaefc7f81
Melody Jimenez <melodyLjimenez@gmail.com>
2023-01-06 15:51:18 -0800

    completed easy LC using hashmap

---
+++ contains-duplicates.py
@@ -0,0 +1,11 @@
+# Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        
+        hashset = set()
+        for num in nums:
+            if num in hashset:
+                return True
+            hashset.add(num)
+
